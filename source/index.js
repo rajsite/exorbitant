bb1572e118822101e967bb5e75e32ad5c185dd1f
rajsite <rajsite@users.noreply.github.com>
2020-11-01 16:18:47 -0600

    Rename js context refs

--- source/index.js
+++ source/index.js
@@ -1,37 +1,45 @@
 import exorbitant from '../dist/exorbitant.js';
 
+// Make sure to use Module.stackSave() before calling
+const writeStringToStack = function (Module, str) {
+    var strMaxStackLength = (str.length << 2) + 1;
+    var strStackPointer = Module.stackAlloc(strMaxStackLength);
+    Module.stringToUTF8(str, strStackPointer, strMaxStackLength);
+    return strStackPointer;
+};
+
 class SymbolTable {
-    constructor (Module, symbolTable) {
+    constructor (Module, symbolTableRef) {
         this._Module = Module;
-        this._symbolTable = symbolTable;
+        this._symbolTableRef = symbolTableRef;
     }
 }
 
 class Expression {
-    constructor (Module, expression) {
+    constructor (Module, expressionRef) {
         this._Module = Module;
-        this._expression = expression;
+        this._expressionRef = expressionRef;
     }
 
     registerSymbolTable (symbolTable) {
-        this._Module._Expression_RegisterSymbolTable(this._expression, symbolTable._symbolTable);
+        this._Module._Expression_RegisterSymbolTable(this._expressionRef, symbolTable._symbolTableRef);
     }
 
     value () {
-        return this._Module._Expression_Value(this._expression);
+        return this._Module._Expression_Value(this._expressionRef);
     }
 }
 
 class Parser {
-    constructor (Module, parser) {
+    constructor (Module, parserRef) {
         this._Module = Module;
-        this._parser = parser;
+        this._parserRef = parserRef;
     }
 
     compile (str, expression) {
         const stack = this._Module.stackSave();
-        const strPtr = this._Module.writeJSStringToStack(str);
-        const ret = this._Module._Parser_Compile(this._parser, strPtr, expression._expression);
+        const strPtr = writeStringToStack(this._Module, str);
+        const ret = this._Module._Parser_Compile(this._parserRef, strPtr, expression._expressionRef);
         this._Module.stackRestore(stack);
         return ret;
     }
@@ -43,31 +51,23 @@ class Exorbitant {
     }
 
     createSymbolTable () {
-        const symbolTable = this._Module._SymbolTable_Create();
-        return new SymbolTable(this._Module, symbolTable);
+        const symbolTableRef = this._Module._SymbolTable_Create();
+        return new SymbolTable(this._Module, symbolTableRef);
     }
 
     createExpression () {
-        const expression = this._Module._Expression_Create();
-        return new Expression(this._Module, expression);
+        const expressionRef = this._Module._Expression_Create();
+        return new Expression(this._Module, expressionRef);
     }
 
     createParser () {
-        const parser = this._Module._Parser_Create();
-        return new Parser(this._Module, parser);
+        const parserRef = this._Module._Parser_Create();
+        return new Parser(this._Module, parserRef);
     }
 }
 
 const createExorbitant = async function () {
     const Module = await exorbitant();
-
-    Module.writeJSStringToStack = function (str) {
-        var strMaxStackLength = (str.length << 2) + 1;
-        var strStackPointer = Module.stackAlloc(strMaxStackLength);
-        Module.stringToUTF8(str, strStackPointer, strMaxStackLength);
-        return strStackPointer;
-    };
-
     return new Exorbitant(Module);
 };
 
