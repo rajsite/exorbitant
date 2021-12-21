58dc859a7e9b54f4eac43458a9fca23d825d1a7c
rajsite <rajsite@users.noreply.github.com>
2021-12-21 00:51:20 -0600

    Add wasi node example

--- example/node-wasi.js
+++ example/node-wasi.js
@@ -2,16 +2,18 @@
 const fs = require('fs');
 const path = require('path');
 const { WASI } = require('wasi');
+const fd = fs.openSync(path.resolve(__dirname, 'node-wasi-input.txt'), 'r');
 const wasi = new WASI({
-    args: process.argv
+    args: process.argv,
+    stdin: fd
 });
 const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
 
 // TODO readline isn't blocking?
 
 (async () => {
-  const wasm = await WebAssembly.compile(fs.readFileSync(path.resolve(__dirname, '../dist/exorbitant.wasm')));
-  const instance = await WebAssembly.instantiate(wasm, importObject);
+    const wasm = await WebAssembly.compile(fs.readFileSync(path.resolve(__dirname, '../dist/exorbitant.wasm')));
+    const instance = await WebAssembly.instantiate(wasm, importObject);
 
-  wasi.start(instance);
+    wasi.start(instance);
 })();
